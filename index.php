427afc1faceb13a175d8b79abaa8afb853c62a97
kn <keiyyy4150@outlook.jp>
2022-09-13 14:38:27 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php 
+
+/* // 配列１行
+$array_1 = [1,2,3];
+
+$array_2 = [
+    ['赤','青','黄'],
+    ['緑','紫','黒']
+];
+
+// echo $array[1];
+
+echo '<pre>';
+var_dump($array_2); // 配列の中身を全て見る
+echo '<pre>';
+
+echo $array_2[1][1]; */
+
+// 連想配列
+$array_member = [
+    'name' => '本田',
+    'height' => 170,
+    'hobby' => 'サッカー'
+];
+
+echo $array_member['hobby'];
+
+echo '<pre>';
+var_dump($array_member);
+echo '<pre>';
+
+$array_member_2 = [
+    '本田' => [
+        'height' => 170,
+        'hobby' => 'サッカー'
+    ],
+    '香川' => [
+        'height' => 165,
+        'hobby' => 'サッカー'
+    ]
+];
+
+echo $array_member_2['香川']['height'];
+
+echo '<pre>';
+var_dump($array_member_2);
+echo '<pre>';
+
+$array_member_3 = [
+    '1組' => [
+        '本田' => [
+            'height' => 170,
+            'hobby' => 'サッカー'
+        ],
+        '香川' => [
+            'height' => 165,
+            'hobby' => 'サッカー'
+        ],
+    ],
+    '2組' => [
+        '長友' => [
+            'height' => 160,
+            'hobby' => 'サッカー'
+        ],
+        '乾' => [
+            'height' => 168,
+            'hobby' => 'サッカー'
+        ],
+    ]
+];
+
+echo $array_member_3['2組']['長友']['height'];
+
+echo '<pre>';
+var_dump($array_member_3);
+echo '<pre>';
+
+?>
