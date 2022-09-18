06841efe620fe69111c67f56ed99fd0c1e02106b
鍋田啓太 <keiyyy4150@outlook.jp>
2022-09-18 23:23:44 +0900

    udemy学習 9/18

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ echo '<pre>';
 echo $array_2[1][1]; */
 
 // 連想配列
-$array_member = [
+/* $array_member = [
     'name' => '本田',
     'height' => 170,
     'hobby' => 'サッカー'
@@ -76,6 +76,47 @@ echo $array_member_3['2組']['長友']['height'];
 
 echo '<pre>';
 var_dump($array_member_3);
-echo '<pre>';
+echo '<pre>'; */
+
+// 比較演算子の学習
+
+/* $test_1 = 7;
+$test_2 = 3;
+
+$test_3 = $test_1 / $test_2;
+
+
+
+echo $test_3; */
+
+// 条件分岐の学習
+
+/* $height = 90;
+
+if ($height === 91) {
+  echo '身長は' . $height . 'cmです';
+} else {
+  echo '身長は' .$height . 'cmではありません';
+} */
+
+$signal = 'blue';
+
+if ($signal === 'red') {
+    echo 'とまれ';
+} else if ($signal === 'yellow') {
+    echo '一旦停止';
+} else { // elseはあまり使用しないほうがいい
+    echo '進む';
+}
+
+echo '<br>';
+
+$speed = 80;
+
+if ($signal === 'blue') {
+    if ($speed >= 80) { // if文の中にif文　ネスト　※見づらいのであまり使用しないほうがいい
+        echo 'スピード違反';
+    }
+} 
 
 ?>
